138786b313f68d5c69a5590f120bae32ce21224b
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-08-11 12:21:40 -0300

    Melhorei o layout com footer e fixei o header na rolagem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
@@ -9,6 +10,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
         <main>
           {children}
         </main>
+        <Footer />
+        
         <script src="https://kit.fontawesome.com/ba7c57d421.js"></script>
       </body>
     </html>
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+import "./footer.css"
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Pedro Rocha Araujo | <i className="fa-regular fa-copyright" aria-hidden="true"></i> 2026</p>
+    </footer>
+  )
+}
