5f7acc9386889eb122b0788efd7cf88f83b8f96b
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-08-11 08:37:02 -0300

    Buscando os filmes da API e guardnado em um state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "dotenv/config"
 
 export default function Home() {
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+    async function getFilmes() {
+      try {
+        const chave = process.env.NEXT_PUBLIC_CHAVE_API
+        const token = process.env.NEXT_PUBLIC_TOKEN_API
+        const response = await axios.get(`https://api.themoviedb.org/3/account/${chave}`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        setFilmes(response.data)
+      } catch(erro) {
+        console.log(erro)
+      }
+    }
+    getFilmes()
+  }, [])
+
   return (
     <>
     </>
