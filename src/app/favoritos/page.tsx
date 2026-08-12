ae33d7e87943fcb2f4d956651dd05fe8daf04fe8
Pedro-Rocha-Araujo <pedrorochaaraujo82@gmail.com>
2026-08-12 11:03:22 -0300

    Estruturei a página de favoritos

--- src/app/favoritos/page.tsx
+++ src/app/favoritos/page.tsx
@@ -1,7 +1,25 @@
+import "./favoritos.css"
+
 export default function Favoritos() {
   return (
-    <section>
-      
-    </section>
+    <div className="container">
+      <section className="favoritos">
+
+        <h2> <i className="fa-regular fa-star" aria-hidden="true"></i> Filmes favoritos</h2>
+
+        <div className="filmes">
+          <div className="filme">
+            <h3>Filme 1</h3>
+            <i className="fa-solid fa-trash" aria-hidden="true"></i>
+          </div>
+
+          <div className="filme">
+            <h3>Filme 2</h3>
+            <i className="fa-solid fa-trash" aria-hidden="true"></i>
+          </div>
+        </div>
+
+      </section>
+    </div>
   )
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,18 @@
+'use client'
+
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import "./header.css"
 
 export default function Header() {
+  const router = useRouter()
+  
   return (
     <header>
       <div className="container">
         <h1> <i className="fa-solid fa-film" aria-hidden="true"></i> <Link href={"/"}>Filmes</Link></h1>
 
-        <i className="fa-solid fa-star fa-xl" aria-hidden="true"></i>
+        <i onClick={()=>router.push("/favoritos")} className="fa-solid fa-star fa-xl" aria-hidden="true"></i>
       </div>
     </header>
   )
